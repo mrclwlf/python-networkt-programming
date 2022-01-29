74e33bf69fcf53f534f257c1e4788fe0c5330a66
Marcel Wolf <wlfmrcl@gmail.com>
2022-01-29 13:37:22 +0100

    server and client closing correctly

--- tcp/client.py
+++ tcp/client.py
@@ -5,33 +5,41 @@ from random import random
 
 def receive_data(sck: socket) -> bytes:
     data = b''
-    while True:
-        msg = sck.recv(4096)
-        data += msg
-        if not msg:
-            return data
+    try:
+        while True:
+            msg = sck.recv(4096)
+            data += msg
+            if not msg:
+                break
+    finally:
+        return data
 
 def client(host: str, port: int) -> None:
-    sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sck.connect((host, port))
-    print('Assigned to', sck.getsockname())
-    data = ['I hear your heart beat to the beat of the drums\n',
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
+        sck.connect((host, port))
+        print('Assigned to', sck.getsockname())
+        data = ['I hear your heart beat to the beat of the drums\n',
             'Oh, what a shame that you came here with someone\n',
             'So while you\'re here in my arms\n',
             'Let\'s make the most of the night like we\'re gonna die young.']
 
-    for elem in data:
-        sck.sendall(bytes(elem, 'utf-8'))
-        print(len(elem))
+        for elem in data:
+            sck.sendall(bytes(elem, 'utf-8'))
+            print(len(elem))
 
 
-    #DEADLOCK
-    #reply = receive_data(sck)
-    #print(reply.decode())
+        #DEADLOCK
+        reply = ''
+        #while not reply:
+            #reply=sck.recv(2048)
+        reply = receive_data(sck)
 
-    sck.close()
-    print('Connection closed')
+        print(reply.decode())
+
+        sck.close()
+        print('Connection closed')
 
 
 if __name__ == '__main__':
-    client(host='localhost', port=42069)
+    for i in range(10):
+        client(host='localhost', port=42069)
--- tcp/server.py
+++ tcp/server.py
@@ -3,6 +3,7 @@ import sys
 
 
 def receive_data(sck: socket) -> bytes:
+    sck.setblocking(False)
     data = b''
     try:
         while True:
@@ -13,23 +14,26 @@ def receive_data(sck: socket) -> bytes:
     #except sck.settimeout(1):
        #print('Timeout')
     finally:
+        sck.setblocking(True)
         return data
 
 def server(host: str, port: int) -> None:
-    sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sck.bind((host, port))
-    sck.listen(1)
-    print('Listening at', sck.getsockname())
-    while True:
-        sc, address = sck.accept()
-        print('Accepted a connection from ', address)
-        print('Socket name: ', sc.getsockname())
-        print('Socket peer: ', sc.getpeername())
-        data = receive_data(sc)
-        print(data.decode('utf-8'))
-        sc.sendall(b'Thanks for your request')
-        sc.close()
-        print('Connection closed')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sck:
+        sck.bind((host, port))
+        #sck.setblocking(False)
+        sck.listen(1)
+        print('Listening at', sck.getsockname())
+        while True:
+            sc, address = sck.accept()
+            print('Accepted a connection from ', address)
+            print('Socket name: ', sc.getsockname())
+            print('Socket peer: ', sc.getpeername())
+            data = receive_data(sc)
+            print(data.decode('utf-8'))
+            sc.sendall(b'Thanks for your request')
+            sc.close()
+            print('Connection closed')
+
 
 
 if __name__ == '__main__':
